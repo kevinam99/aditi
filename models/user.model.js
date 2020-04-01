8da8fc907a16b8230a56cd4b06fe28d098b2eb2c
kevinam99 <kevinam99@gmail.com>
2020-04-01 20:24:40 +0530

    User schema

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema= new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+
+}, {
+    timestamps: true,
+
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
