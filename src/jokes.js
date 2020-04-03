e1545962b59453f7105ce7e5cdf99dffbf43eea3
kevinam99 <kevinam99@gmail.com>
2020-04-03 17:40:36 +0530

    Jokes module complete. Integration remaining

--- src/jokes.js
+++ src/jokes.js
@@ -1,9 +1,3 @@
-const apis = {
-    "chuck": "http://api.icndb.com/jokes/random",
-    "random": "https://official-joke-api.appspot.com/random_joke" {setup, punchline},
-    "jokeoftheday": "https://api.jokes.one/jod"
-}
-
 const axios = require('axios')
 
 const getChuckNorrisJoke = () =>
@@ -11,10 +5,47 @@ const getChuckNorrisJoke = () =>
     return new Promise((resolve, reject) => {
         axios.get("http://api.icndb.com/jokes/random")
              .then(response => {
-                 console.log(response);
+                 console.log(response.data.value.joke);
+                 resolve(response.data.value.joke)
              })
-             .catch(err => console.log(err))
+             .catch(err => {
+                console.log(err)
+                reject(err)
+             })
     })
 }
 
+const getrandomJoke = () => {
+    return new Promise((resolve, reject) => {
+        axios.get("https://official-joke-api.appspot.com/random_joke")
+             .then(response => {
+                 const joke = `${response.data.setup} \n ${response.data.punchline}`
+                 console.log(joke);
+                 resolve(joke)
+             })
+             .catch(err => {
+                 console.log(err)
+                 reject(err)
+             })
+    })
+}
+
+const getJokeOfTheDay = () => {
+    return new Promise((resolve, reject) => {
+        axios.get("https://api.jokes.one/jod")
+             .then(response => {
+                //  const joke = `${response.data.setup} \n ${response.data.punchline}`
+                 console.log(response.data.contents.jokes[0].joke.text);
+                //  resolve(joke)
+             })
+             .catch(err => {
+                 console.log(err)
+                 reject(err)
+             })
+    })
+}
+
+
 getChuckNorrisJoke();
+getrandomJoke();
+getJokeOfTheDay();
