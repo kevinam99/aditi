b127da0eb28674e22b5d680071acb791f6a442e5
kevinam99 <kevinam99@gmail.com>
2020-04-12 16:01:45 +0530

    dist/

---
+++ dist/models/user.model.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+}, {
+    timestamps: true,
+});
+const User = mongoose.model('User', userSchema);
+exports.default = User;
